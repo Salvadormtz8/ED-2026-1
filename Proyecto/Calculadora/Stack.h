380b2b7a6562f1b720271d431556552da9893d05
Salvadormtz8 <chaarawys28@gmail.com>
2025-12-02 17:46:26 -0600

    Add stack header file with basic operations

--- Proyecto/Calculadora/Stack.h
+++ Proyecto/Calculadora/Stack.h
@@ -1,1 +1,17 @@
+#ifndef STACK_H
+#define STACK_H
+#define MAX 1000
 
+typedef struct {
+    char elementos[MAX][MAX];
+    int cima;
+} Pila;
+
+void inicializarPila(Pila *p);
+int pilaLlena(Pila *p);
+int pilaVacia(Pila *p);
+void apilar(Pila *p, const char *elemento);
+char *desapilar(Pila *p);
+char *consultarCima(Pila *p);
+
+#endif
