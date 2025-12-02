6a8add538301ae6478c5120bcee511f6e3ae9cfd
Salvadormtz8 <chaarawys28@gmail.com>
2025-12-02 17:47:29 -0600

    Add stack operations in Stack.c
    Implemented stack operations including initialization, push, pop, and top element retrieval.

--- Proyecto/Calculadora/Stack.c
+++ Proyecto/Calculadora/Stack.c
@@ -1,1 +1,22 @@
+#include "Stack.h"
+#include <string.h>
+#include <stdio.h>
 
+void inicializarPila(Pila *p) { p->cima = -1; }
+int pilaLlena(Pila *p) { return p->cima == MAX - 1; }
+int pilaVacia(Pila *p) { return p->cima == -1; }
+
+void apilar(Pila *p, const char *elemento) {
+    if (!pilaLlena(p)) {
+        p->cima++;
+        strcpy(p->elementos[p->cima], elemento);
+    }
+}
+
+char *desapilar(Pila *p) {
+    return pilaVacia(p) ? NULL : p->elementos[p->cima--];
+}
+
+char *consultarCima(Pila *p) {
+    return pilaVacia(p) ? NULL : p->elementos[p->cima];
+}
