2f8876802b2aa9baebebece7b92358d71e61c624
Salvadormtz8 <chaarawys28@gmail.com>
2025-12-02 17:48:56 -0600

    Add BigNumber struct and function declarations
    Define structures and functions for BigNumber operations.

--- Proyecto/Calculadora/Dlist.h
+++ Proyecto/Calculadora/Dlist.h
@@ -1,1 +1,23 @@
+#ifndef DLIST_H
+#define DLIST_H
 
+typedef struct Nodo {
+    char digito;
+    struct Nodo *anterior;
+    struct Nodo *siguiente;
+} Nodo;
+
+typedef struct {
+    Nodo *inicio;
+    Nodo *fin;
+    int decimales;
+    int negativo;
+} BigNumber;
+
+BigNumber* crearBigNumber();
+void liberarBigNumber(BigNumber *num);
+void agregarDigito(BigNumber *num, char digito);
+void imprimirBigNumber(BigNumber *num);
+BigNumber* stringToBigNumber(const char *str);
+
+#endif
