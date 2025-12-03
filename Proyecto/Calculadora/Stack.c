f74ab5a9c499429d49eb7a8b1ba68fb8234fbda5
Salvadormtz8 <chaarawys28@gmail.com>
2025-12-02 18:06:37 -0600

    Update Stack.c

--- Proyecto/Calculadora/Stack.c
+++ Proyecto/Calculadora/Stack.c
@@ -1,22 +1,52 @@
-#include "Stack.h"
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "stack.h"
 
-void inicializarPila(Pila *p) { p->cima = -1; }
-int pilaLlena(Pila *p) { return p->cima == MAX - 1; }
-int pilaVacia(Pila *p) { return p->cima == -1; }
+void inicializarPila(Pila *pila) {
+    pila->cima = -1;
+}
 
-void apilar(Pila *p, const char *elemento) {
-    if (!pilaLlena(p)) {
-        p->cima++;
-        strcpy(p->elementos[p->cima], elemento);
+int pilaLlena(Pila *pila) {
+    return pila->cima == MAX - 1;
+}
+
+int pilaVacia(Pila *pila) {
+    return pila->cima == -1;
+}
+
+void apilar(Pila *pila, const char *elemento) {
+    if (pilaLlena(pila)) {
+        fprintf(stderr, "Error: La pila está llena.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        pila->cima++;
+        strcpy(pila->elementos[pila->cima], elemento);
     }
 }
 
-char *desapilar(Pila *p) {
-    return pilaVacia(p) ? NULL : p->elementos[p->cima--];
+char *desapilar(Pila *pila) {
+    static char elemento[MAX];
+    if (pilaVacia(pila)) {
+        fprintf(stderr, "Error: La pila está vacía.\n");
+        return NULL;
+    } else {
+        strcpy(elemento, pila->elementos[pila->cima]);
+        pila->cima--;
+        return elemento;
+    }
 }
 
-char *consultarCima(Pila *p) {
-    return pilaVacia(p) ? NULL : p->elementos[p->cima];
+char *consultarCima(Pila *pila) {
+    static char elemento[MAX];
+    if (pilaVacia(pila)) {
+        return NULL;
+    } else {
+        strcpy(elemento, pila->elementos[pila->cima]);
+        return elemento;
+    }
+}
+
+void limpiarPila(Pila *pila) {
+    pila->cima = -1;
 }
